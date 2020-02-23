4555bfc772acf3586bd5d48f5cd0fd49cdb9f5bd
David <arattanasack@gmail.com>
2020-02-22 16:25:39 -0800

    adds travis.yml

--- server.js
+++ server.js
@@ -14,45 +14,52 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://user:password1@ds213896.mlab.com:13896/heroku_qtvczxd3", { useNewUrlParser: true });
 
-// A GET route for scraping the Apex Legends update page
+// A GET route for scraping the keysets from originativeco
 app.get('/scrape', (req, res) =>{
-    axios.get('https://www.ea.com/games/apex-legends/news')
+    axios.get('https://www.originativeco.com/collections/keysets')
     .then((response) => {
         let $ = cheerio.load(response.data);
-        let result = {};
-        $('eapl-tile__title h5').each((i, element) =>{
+        $('li .title').each((i, element) =>{
+            let result = {};
 
-            result.title = $(this)
-            // .children('a')
+            result.title = $(element)
+            .children('a')
             .text();
 
-            result.link = $(this)
+            result.link = $(element)
             .children()
             .attr('href');
 
-            // db.Article.create(result)
-            // .then((dbArticle) => {
-            //     console.log(dbArticle);
-            // })
-            // .catch((err) => {
-            //     res.send(err);
-            // });
-
-            result.push({
-                title: title,
-                link: link
+            db.Article.create(result)
+            .then((dbArticle) => {
+                console.log(dbArticle);
+            })
+            .catch((err) => {
+                res.send(err);
             });
         });
-        console.log(result);
-        res.json(result);
+        console.log(dbArticle);
     });
-    // res.send('Scrape Complete');
+    res.send('Scrape Complete');
 });
 
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + "/public/html/index.html")
-})
+});
+
+app.get('/keysets', (req, res)=> {
+    db.Article
+    .find({})
+    .then((response) => {
+        res.json(response);
+    })
+    .catch((err) => {
+        if (err) {
+            res.json(err);
+        }
+    });
+});
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port: ${PORT}`);
